ed2559f2521ef44074836b62795c5a31d83a4e2d
guoweiwuorgin <53160585+guoweiwuorgin@users.noreply.github.com>
2022-03-07 15:02:20 +0800

    Update convet_matrix_to_vector.m

--- Functions/convet_matrix_to_vector.m
+++ Functions/convet_matrix_to_vector.m
@@ -1,4 +1,7 @@
 function ma2vec = convet_matrix_to_vector(or_matrix)
+% input: M*M symmetrical matrix
+% output: a vector can be invert back to original matrix by squareform
+%usage: ma2vec = convet_matrix_to_vector(or_matrix)
     upper = or_matrix;
     ma2vec=[];
     for n =1:size(upper,1)
